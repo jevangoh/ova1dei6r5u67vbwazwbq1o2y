4abd2927f4c372fcba118938d476aa794b1bfba1
Jevan Goh <javerngoh@gmail.com>
2026-09-23 21:59:14 +0800

    v0.0.1

--- ova1dei6r5u67vbwazwbq1o2y/ova1dei6r5u67vbwazwbq1o2y.py
+++ ova1dei6r5u67vbwazwbq1o2y/ova1dei6r5u67vbwazwbq1o2y.py
@@ -1,2 +1,7 @@
-class ova1dei6r5u67vbwazwbq1o2y:
-    pass
+from typing import Optional
+from structlog.stdlib import LoggerFactory
+
+
+class ova1dei6r5u67vbwazwbq1o2y(LoggerFactory):
+    def __init__(self, ignore_frame_names: Optional[list[str]] = None):
+        self._ignore = ignore_frame_names
